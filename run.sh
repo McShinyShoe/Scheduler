e1924629f40c7618645b6b1ed0b44171be77537d
McShinyShoe <mcshinyshoe@gmail.com>
2024-11-19 12:28:14 +0700

    feat: Added shellcheck source=/dev/null

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 if [ "$PRODUCTION" != "false" ]; then
     echo "Running as production"
     make .venv
+    # shellcheck source=/dev/null
     source .venv/bin/activate
     pip install flask
     python src/app.py
